21245a34bdfe07dacdd78118c202986761a2366e
H1vee <ailedinskyi@gmail.com>
2024-04-03 21:40:29 +0300

    add first task

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Lab9_10CharpT
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                   Task1 task1 = new Task1();
+                    Console.WriteLine(task1.task("abc#d##c"));
+                    break;  
+            }
+            
+            
+        }
+    }
+}
---
+++ Lab9_10CharpT/Task1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT
+{
+    internal class Task1
+    {
+        public string task(string text)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in text)
+            {
+                if (c != '#')
+                {
+                    stack.Push(c);
+                }
+                else if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+
+            return string.Join("", stack.Reverse());
+        }
+    }
+}
